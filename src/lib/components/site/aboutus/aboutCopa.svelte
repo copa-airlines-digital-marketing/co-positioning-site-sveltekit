f81c63216d8b65719aee238178cf538e146ecc1d
César Patiño <cpatino@copaair.com>
2024-05-30 10:28:21 -0500

    about copa main pilars done

--- src/lib/components/site/aboutus/aboutCopa.svelte
+++ src/lib/components/site/aboutus/aboutCopa.svelte
@@ -10,6 +10,13 @@
 	import * as Slide from '../slide';
 	import * as EnhancedImage from '$lib/components/ui/image';
 	import mainImage from '$lib/assets/sample.jpg?w=3840;2560;1920;1560;1366;1024;720&format=avif;webp;jpg&as=picture&imagetools';
+	import puntualidad from '$lib/assets/abordaje-de-vuelo.jpg?w=3840;2560;1920;1560;1366;1024;720&format=avif;webp;jpg&as=picture&imagetools';
+	import puntualidadPortrait from '$lib/assets/abordaje-de-vuelo-portrait.jpg?w=3840;2560;1920;1560;1366;1024;720&format=avif;webp;jpg&as=picture&imagetools';
+	import conectividad from '$lib/assets/persona-revisando-celular-con-tabla-de-vuelos-detras.jpg?w=3840;2560;1920;1560;1366;1024;720&format=avif;webp;jpg&as=picture&imagetools';
+	import conectividadPortrait from '$lib/assets/persona-revisando-celular-con-tabla-de-vuelos-detras-portrait.jpg?w=3840;2560;1920;1560;1366;1024;720&format=avif;webp;jpg&as=picture&imagetools';
+	import connectmiles from '$lib/assets/counter-copa-airlines-revision-de-documentos.jpg?w=3840;2560;1920;1560;1366;1024;720&format=avif;webp;jpg&as=picture&imagetools';
+	import connectmilesPortrait from '$lib/assets/counter-copa-airlines-revision-de-documentos-portrait.jpg?w=3840;2560;1920;1560;1366;1024;720&format=avif;webp;jpg&as=picture&imagetools';
+
 	import { Heading } from '$lib/components/ui/heading';
 	import { Button } from '$lib/components/ui/button';
 	import Autoplay from 'embla-carousel-autoplay';
@@ -24,20 +31,23 @@
 			'(orientation: portrait)': mainImage
 		},
 		connectmiles: {
-			'(orientation: landscape)': mainImage,
-			'(orientation: portrait)': mainImage
+			'(orientation: portrait)': connectmilesPortrait,
+			'(orientation: portait) and (min-width: 1367px)': connectmiles,
+			'(orientation: landscape)': connectmiles
 		},
 		puntualidad: {
-			'(orientation: landscape)': mainImage,
-			'(orientation: portrait)': mainImage
+			'(orientation: portrait)': puntualidadPortrait,
+			'(orientation: portait) and (min-width: 1367px)': puntualidad,
+			'(orientation: landscape)': puntualidad
 		},
 		copaClub: {
 			'(orientation: landscape)': mainImage,
 			'(orientation: portrait)': mainImage
 		},
 		conectividad: {
-			'(orientation: landscape)': mainImage,
-			'(orientation: portrait)': mainImage
+			'(orientation: portrait)': conectividadPortrait,
+			'(orientation: portait) and (min-width: 1367px)': conectividad,
+			'(orientation: landscape)': conectividad
 		},
 		reconocimientos: {
 			'(orientation: landscape)': mainImage,
@@ -58,7 +68,7 @@
 		<span class="rounded-full block overflow-hidden bg-common-white">
 			<IstoipoCopaAirlines class="size-6"></IstoipoCopaAirlines>
 		</span>
-		Descubre Copa Airlines
+		<span class="text-common-white">Descubre Copa Airlines</span>
 	</Heading>
 	<Carousel.Root
 		class="col-span-full row-span-full grid grid-cols-subgrid grid-rows-subgrid relative"
@@ -83,9 +93,9 @@
 					slide={index}
 					class="w-full max-w-none p-0 m-0 bg-transparent grid grid-cols-1 grid-rows-1 border-0"
 				>
-					<div class="bg-secondary/60 row-start-1 row-end-1 h-1 w-full my-3">
+					<div class="bg-common-white/60 row-start-1 row-end-1 h-1 w-full my-3">
 						{#if selected === index}
-							<div class="h-full w-full bg-secondary"></div>
+							<div class="h-full w-full bg-common-white"></div>
 						{/if}
 					</div>
 				</Carousel.Dot>
@@ -122,9 +132,10 @@
 						<Slide.Content class="row-start-3">
 							<div class="row-span-full col-span-full my-normal text-common-white">
 								<Heading variant="displayNormal" tag="h3" class="text-common-white">
-									<span class="text-secondary-faded">75 Años</span> Volando Los Cielos De Las Américas
+									Más De <span class="text-secondary-faded">75 Años</span> Volando Los Cielos De Las
+									Américas
 								</Heading>
-								<p class="mt-2 mb-6 max-w-prose">
+								<p class="my-2 max-w-prose">
 									Hemos estado conectando las Américas por 75 años y aqui hay algunas razones por
 									las que somos los mejores
 								</p>
@@ -134,15 +145,22 @@
 				</Carousel.Item>
 				<Carousel.Item>
 					<Slide.About image={images['puntualidad']} class="relative">
-						<Slide.Overlay />
+						<Slide.Overlay
+							class="bg-gradient-to-t from-20% portrait:sm:bg-gradient-to-b landscape:md:bg-gradient-to-r"
+						/>
 						<Slide.Content class="row-start-3">
-							<div class="row-span-full col-span-full my-normal text-common-white">
-								<Heading variant="displayNormal" tag="h3" class="text-common-white"
+							<div
+								class="row-start-12 row-end-1 landscape:row-start-13 portrait:sm:flex-col landscape:md:flex-col col-span-full my-normal text-common-white flex flex-col-reverse"
+							>
+								<Heading
+									variant="displayNormal"
+									tag="h3"
+									class="text-common-white landscape:md:max-w-sm"
 									>La Aerolínea Más Premiada Por <span class="text-secondary-faded"
 										>Puntualidad</span
 									> En La Última Década</Heading
 								>
-								<p class="mt-3 mb-6 max-w-prose">
+								<p class="my-2 max-w-prose landscape:md:max-w-sm">
 									Hemos sido reconocidos por Cirium como la aerolínea más puntual de Latinoamérica
 									en 9 ocasiones durante la última década.
 								</p>
@@ -152,13 +170,20 @@
 				</Carousel.Item>
 				<Carousel.Item>
 					<Slide.About image={images['conectividad']} class="relative">
-						<Slide.Overlay />
+						<Slide.Overlay
+							class="bg-gradient-to-b landscape:bg-gradient-to-t landscape:md:bg-gradient-to-r"
+						/>
 						<Slide.Content class="row-start-3">
-							<div class="row-span-full col-span-full my-normal text-common-white">
-								<Heading variant="displayNormal" tag="h3" class="text-common-white"
+							<div
+								class="row-start-12 row-end-1 col-span-full my-normal text-common-white flex flex-col landscape:flex-col-reverse landscape:row-start-13 landscape:md:flex-col landscape:md:from-15%"
+							>
+								<Heading
+									variant="displayNormal"
+									tag="h3"
+									class="text-common-white landscape:md:max-w-sm landscape:lg:max-w-lg"
 									>Conexiones Rápidas a Través Del Hub De Las Américas® En Panamá</Heading
 								>
-								<p class="mt-3 mb-6 max-w-prose">
+								<p class="my-2 max-w-prose landscape:md:max-w-sm">
 									Nuestras conexiones son fáciles y rápidas, sin trámites de migración ni aduana y
 									con el equipaje registrado hasta tu destino final.
 								</p>
@@ -168,23 +193,25 @@
 				</Carousel.Item>
 				<Carousel.Item>
 					<Slide.About image={images['connectmiles']} class="relative">
-						<Slide.Overlay />
+						<Slide.Overlay class="bg-gradient-to-b to-30% landscape:md:to-25%" />
+						<Slide.Overlay
+							class="bg-gradient-to-t from-15% landscape:md:from-0% landscape:md:to-30%"
+						/>
 						<Slide.Content class="row-start-3">
-							<div class="row-span-full col-span-full my-normal text-common-white">
+							<div
+								class="row-start-12 row-end-1 col-span-full my-normal text-common-white flex flex-col-reverse items-start landscape:md:row-start-13"
+							>
 								<Heading variant="displayNormal" tag="h3" class="text-common-white"
 									>Acumula y redime millas</Heading
 								>
-								<p class="mt-3 mb-6 max-w-prose">
-									Únete gratis a nuestro Programa de Lealtad <strong>ConnectMiles</strong> y
-									comienza a ganar millas cuando vueles con Copa Airlines, las más de 25 aerolíneas
-									miembros de Star Alliance, otras aerolíneas socias y con todos nuestros socios
-									comerciales: hoteles, empresas de alquiler de autos, entre otros.<br />También, si
-									te conviertes en PreferMember, podrás disfrutar de increíbles beneficios, como
-									ascensos de categoría, Priority Pass, franquicia de equipaje adicional, acceso a
-									salas VIP y mucho más.
+								<p class="my-2 max-w-prose">
+									Únete gratis a nuestro Programa de Lealtad <strong>ConnectMiles</strong> y acumula
+									y redime millas en las +25 aerolíneas miembro de Star Alliance.
 								</p>
-								<Button href="https://www.copaair.com/es-gs/enrollment/" target="_blank"
-									>Únete ya</Button
+								<Button
+									class="my-3 bg-secondary border-secondary"
+									href="https://www.copaair.com/es-gs/enrollment/"
+									target="_blank">Únete ya</Button
 								>
 							</div>
 						</Slide.Content>
@@ -198,7 +225,7 @@
 								<Heading variant="displayNormal" tag="h3" class="text-common-white"
 									>Miembro de Star Alliance</Heading
 								>
-								<p class="mt-2 mb-6 max-w-prose">
+								<p class="my-2 max-w-prose">
 									Somos parte de Star Alliance, líder de la red global de aerolíneas. Puedes ganar y
 									redimir millas aéreas en más de 25 aerolíneas miembros de la red, y tus millas
 									acumuladas te ayudan a mejorar tu status y disfrutar de todos los beneficios
@@ -217,7 +244,7 @@
 								<Heading variant="displayNormal" tag="h3" class="text-common-white"
 									><span class="text-secondary-faded">Copa Club</span>, 6 Salas en 5 Aeropuertos</Heading
 								>
-								<p class="mt-3 mb-6 max-w-prose">
+								<p class="my-2 max-w-prose">
 									En Copa Airlines entendemos que tus negocios no esperan mientras viajas, por lo
 									que te brindamos facilidades para atenderte cómodamente, teniendo a su vez, la
 									oportunidad de relajarte y descansar en un ambiente exclusivo. <br /> <br />
@@ -226,6 +253,7 @@
 								</p>
 
 								<Button
+									class="my-4 bg-secondary border-secondary"
 									href="https://www.copaair.com/es-co/descubre-copa-airlines/experiencia-en-aeropuerto/copa-club/"
 									target="_blank">Conoce más</Button
 								>
@@ -234,7 +262,7 @@
 					</Slide.About>
 				</Carousel.Item>
 				<Carousel.Item>
-					<Slide.About image={images['conectividad']} class="relative">
+					<Slide.About image={images['experiencia']} class="relative">
 						<Slide.Overlay />
 						<Slide.Content class="row-start-3">
 							<div class="row-span-full col-span-full my-normal text-common-white">
